b3ffece31373d4f1b82d08001845a0b18e500b19
Tabea Soelter <tabea.soelter@gmail.com>
2025-10-15 16:03:03 -0500

    Include driver to R script

---
+++ src/figures/kidney_marvel_figures.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1          
+#SBATCH --time=1:00:00                    
+#SBATCH --mem=64G
+#SBATCH --partition=express
+#SBATCH --output=%x_%A_%a.out
+#SBATCH --error=%x_%A_%a.err
+
+########################################
+### PUT YOUR COMMANDS BELOW THIS BOX ###
+########################################
+PROJECT_PATH="$(cd "$SLURM_SUBMIT_DIR/../.." && pwd)"
+
+module load Singularity/3.5.2-GCC-5.4.0-2.26
+
+singularity exec --cleanenv \
+                 --containall \
+                 -B "${PROJECT_PATH}" \
+                 "${PROJECT_PATH}"/bin/docker/setbp1_alternative_splicing_1.0.9.sif \
+                 Rscript --vanilla "${PROJECT_PATH}"/src/figures/kidney_marvel_figures.R \
+                 "${PROJECT_PATH}"
